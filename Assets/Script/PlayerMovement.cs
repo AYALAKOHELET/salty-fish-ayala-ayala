177f579929ec2c3f6b2ba1550ef06c2e2b3b92df
AYALAKOHELET <A0556764032@GMAIL.COM>
2021-12-02 13:17:07 +0200

    UPDATE PROJECT

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [SerializeField] private int speed;
+
+    private float vInput;
+    private float hInput;
+
+    private const string VERTICAL_AXIS = "Vertical";
+    private const string HORIZPNTAL_AXIS = "Horizontal";
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+   
+    private void Update()
+    {
+        vInput = Input.GetAxis(VERTICAL_AXIS) * speed;
+        hInput = Input.GetAxis(HORIZPNTAL_AXIS) * speed;
+
+        Vector3 move = new Vector3(hInput, 0, vInput);
+        rb.AddForce(move * speed * Time.deltaTime);
+    }
+
+    
+}
