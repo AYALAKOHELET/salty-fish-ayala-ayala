6ef33995353df5e5ce6bd3d697085eda6946c194
AYALAKOHELET <A0556764032@GMAIL.COM>
2022-01-03 21:09:47 +0200

    Camera surveillance on the player and changes and some visuals

---
+++ Assets/Script/CameraPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform playerTransform;
+    private Vector3 offset;
+
+            // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - playerTransform.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = playerTransform.position + offset;
+    }
+}
